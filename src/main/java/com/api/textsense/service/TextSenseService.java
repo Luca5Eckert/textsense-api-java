aac9b668a7c1dffc9017d02754c202373aa0e5ae
Luca5Eckert <ecklukatv@gmail.com>
2025-11-10 19:10:28 -0300

    feat: adiciona TextSenseService

--- src/main/java/com/api/textsense/service/TextSenseService.java
+++ src/main/java/com/api/textsense/service/TextSenseService.java
@@ -1,17 +1,31 @@
 package com.api.textsense.service;
 
 import com.api.textsense.dto.request.TextSenseRequest;
-import com.api.textsense.dto.response.TextSenseResponse;
+import com.api.textsense.model.Sentiment;
+import com.api.textsense.model.Statistics;
+import com.api.textsense.model.TextSense;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class TextSenseService {
 
     private final StatisticsProvider statisticsProvider;
+    private final SentimentProvider sentimentProvider;
+    private final KeyWordProvider keyWordProvider;
 
-    public TextSenseService(StatisticsProvider statisticsProvider) {
+    public TextSenseService(StatisticsProvider statisticsProvider, SentimentProvider sentimentProvider, KeyWordProvider keyWordProvider) {
         this.statisticsProvider = statisticsProvider;
+        this.sentimentProvider = sentimentProvider;
+        this.keyWordProvider = keyWordProvider;
     }
 
-    public TextSenseResponse analyse(TextSenseRequest textSenseRequest){
-        return null;
+    public TextSense analyse(TextSenseRequest textSenseRequest){
+        Statistics statistics = statisticsProvider.of(textSenseRequest.text());
+        Sentiment sentiment = sentimentProvider.of(textSenseRequest.text());
+        List<String> keywords = keyWordProvider.of(textSenseRequest.text());
+
+        return new TextSense(statistics, sentiment, keywords);
     }
 }
