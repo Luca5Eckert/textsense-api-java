ff8fcfb27a3004f9d3529efc6d3f737795a5f764
Luca5Eckert <ecklukatv@gmail.com>
2025-11-10 16:47:40 -0300

    chore: add base of project

---
+++ src/main/java/com/api/textsense/TextsenseApplication.java
@@ -0,0 +1,13 @@
+package com.api.textsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextsenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/textsense/TextsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.textsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
