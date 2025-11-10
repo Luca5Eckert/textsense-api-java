0d490d0f19ab9d34ad4ac3936e478ad35350b598
Luca5Eckert <ecklukatv@gmail.com>
2025-11-10 16:55:48 -0300

    feat: create TextoSenseController

--- src/main/java/com/api/textsense/controller/TextSenseController.java
+++ src/main/java/com/api/textsense/controller/TextSenseController.java
@@ -1,4 +1,19 @@
 package com.api.textsense.controller;
 
+import com.api.textsense.dto.request.TextSenseRequest;
+import com.api.textsense.dto.response.TextSenseResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TextSenseController {
+
+    @GetMapping("/analyse")
+    public ResponseEntity<TextSenseResponse> analyse(@Valid TextSenseRequest textSenseRequest){
+        return ResponseEntity
+                .ok(new TextSenseResponse(null, null, null));
+    }
+
 }
